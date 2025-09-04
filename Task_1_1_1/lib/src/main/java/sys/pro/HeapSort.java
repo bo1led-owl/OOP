edd5ce6145ee13cd8e0fe6dd2702a22b3b561f98
Ilya Sovtsov <ilyasovty@gmail.com>
2025-09-04 10:22:50 +0700

    format

--- Task_1_1_1/lib/src/main/java/sys/pro/HeapSort.java
+++ Task_1_1_1/lib/src/main/java/sys/pro/HeapSort.java
@@ -1,21 +1,21 @@
 package sys.pro;
 
 public class HeapSort {
-  static private int parentIndex(int i) {
+  private static int parentIndex(int i) {
     return (i + 1) / 2 - 1;
   }
 
-  static private int leftChildIndex(int i) {
+  private static int leftChildIndex(int i) {
     return (i + 1) * 2 - 1;
   }
 
-  static private void swap(int[] items, int i, int j) {
+  private static void swap(int[] items, int i, int j) {
     int tmp = items[i];
     items[i] = items[j];
     items[j] = tmp;
   }
 
-  static private void siftDown(int[] items, int i, int end) {
+  private static void siftDown(int[] items, int i, int end) {
     while (leftChildIndex(i) < end) {
       int child = leftChildIndex(i);
       if (child + 1 < end && items[child] < items[child + 1]) {
@@ -31,13 +31,13 @@ public class HeapSort {
     }
   }
 
-  static private void heapify(int[] items) {
+  private static void heapify(int[] items) {
     for (int i = items.length - 1; i >= 0; --i) {
       siftDown(items, i, items.length - i);
     }
   }
 
-  static public void sort(int[] items) {
+  public static void sort(int[] items) {
     heapify(items);
 
     for (int i = items.length - 1; i > 0; --i) {
--- Task_1_1_1/lib/src/test/java/sys/pro/HeapSortTest.java
+++ Task_1_1_1/lib/src/test/java/sys/pro/HeapSortTest.java
@@ -1,10 +1,11 @@
 package sys.pro;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class HeapSortTest {
-  static private boolean isSorted(int[] arr) {
+  private static boolean isSorted(int[] arr) {
     for (int i = 0; i < arr.length - 1; ++i) {
       if (arr[i] > arr[i + 1]) {
         return false;
@@ -13,12 +14,12 @@ public class HeapSortTest {
 
     return true;
   }
-  
+
   @Test
   public void testSort() {
-    int[] array = new int[] { 3, 1, 5, 4, 2 };
+    int[] array = new int[] {3, 1, 5, 4, 2};
     HeapSort.sort(array);
-    
+
     assertTrue(isSorted(array));
   }
 }
