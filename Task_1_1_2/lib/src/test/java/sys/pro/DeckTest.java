0cef0348d8dd65ef67cd95a066e094a1e02a54c1
Ilya Sovtsov <ilyasovty@gmail.com>
2025-09-16 17:37:22 +0700

    actually fix build?????

---
+++ Task_1_1_2/lib/src/test/java/sys/pro/DeckTest.java
@@ -0,0 +1,12 @@
+package sys.pro;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class DeckTest {
+    @Test
+    void foo() {
+        assertTrue(true);
+    }
+}
