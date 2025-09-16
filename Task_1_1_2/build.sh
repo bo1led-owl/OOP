8645105516645bc7be6909bd66f27103f39d708c
Ilya Sovtsov <ilyasovty@gmail.com>
2025-09-16 17:19:44 +0700

    actually fix build???

--- Task_1_1_2/build.sh
+++
@@ -1,3 +0,0 @@
-javac -d ./build/classes/ ./src/main/java/sys/pro/*.java ./src/main/java/sys/pro/*/*.java
-jar -c -f ./build/Blackjack.jar -e sys.pro.Game -C ./build/classes .
-java -jar ./build/Blackjack.jar
